bea1c7f10a1be2e6c44a9cddfa3f5f7e9ed0d4f5
unknown <mazda@.(none)>
2011-06-17 19:45:43 +0200

    moved to use Sinon.js

--- spec/GoogleTasksSpec.js
+++ spec/GoogleTasksSpec.js
@@ -1,33 +1,65 @@
 /**
  * @author mazda
  */
-describe("GoogleTasksAPI", function() {
-  it("can authenticate with google", function() {
-    var spy = sinon.spy();
-	var gt = new GoogleTasks();   
-	runs(function () {
-	  gt.authenticate(spy);
-	});
-	waits(1000);
-	runs(function () {
-	  expect (spy.called).toBeTruthy();
-  	});
+describe("GoogleTasksAPI", function () {
+	
+	var server;
+	var localStorage_get;
+	var localStorage_set;
+	
+  beforeEach(function () {
+  	// Refresh Key is held in local storage - so let's stub that
+	localStorage_get = sinon.stub(localStorage, "getItem");
+	localStorage_set = sinon.stub(localStorage, "setItem");
+	localStorage_stub.withArgs("GoogleRefreshKey").returns("STUBBED_REFRESH_KEY");
+	// now let's repalce the XHR server so we can mock up Google
+	server = sinon.fakeServer.create();
   });
   
-  it("can can retreive a shopping list from Google", function() {
-  	var gt = new GoogleTasks();  
-	  var shoppingList;
-	  runs(function () {
-	  gt.authenticate(function(){
-	  	gt.fetchShoppingList(function(sl){
-			shoppingList= sl;
-		});
-	  });
+  afterEach(function () {
+    localStorage_get.restore();
+	localStorage_set.restore();
+	server.restore();
+  });
+	
+  it("can authenticate with google when refresh key is in local storage", sinon.test(function() {
+    var spy = sinon.spy();
+	server.respondWith(
+	
+	);
+
+	
+	// now we need a fake XHR server to represent the authentication server
+	
+	var gt = window.GoogleTasks;   
+	gt.eventServer.on(gt.ACCESS_TOKEN_REFRESHED_EVENT, spy);
+	runs(function () {
+	  gt.authenticate();
 	});
 	waits(1000);
 	runs(function () {
-	  expect (sl).toBeDefined();
+	  expect (spy.called).toHaveBeenCalled();
+	  expect (gt.isAuthenticated()).toBeTruthy();
   	});
-  });
+  }));
+  
+  it("can can retreive a shopping list from Google", sinon.test(function () {
+  	var gt = window.GoogleTasks;  
+	var shoppingList;
+	runs(function () {
+		gt.authenticate();
+	});
+	while(!gt.isAuthenticated()){
+		waits(50);
+	}
+	runs(function () {
+		gt.eventServer.on(gt.TASK_LIST_RETREIVED, function(sl){shoppingList = sl;});
+		gt.retreiveTaskList();
+	});
+	waits(1000);
+	runs(function () {
+	  expect (shoppingList).toBeDefined();
+  	});
+  }));
   
 });
--- src/GoogleTasks.js
+++ src/GoogleTasks.js
@@ -9,78 +9,121 @@ Redirect URIs:	urn:ietf:wg:oauth:2.0:oob
 4/sLcLFEN7pJyJhbBSo3S5PHy9NhwB
  */
 
-
-function GoogleTasks(){
-	this.clientId = "256653011780.apps.googleusercontent.com";
-    this.clientSecret = "R14-a9fiYWMx6PwXvxec_hlN";
+window.GoogleTasks = function (){
+	var clientId = "256653011780.apps.googleusercontent.com";
+    var clientSecret = "R14-a9fiYWMx6PwXvxec_hlN";
 
     // Or your redirect URL for web based applications.
-    this.redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
-    this.scope = "https://www.googleapis.com/auth/tasks";
-	this.access_token = "";
-	this.refresh_token = "";
-}
-
-
-
-GoogleTasks.prototype.authenticate = function(success){
-	var storedRefreshToken = localStorage.getItem("GoogleRefreshKey");
-	if (storedRefreshToken != undefined) {
-		this.refresh_token = storedRefreshToken;
-		this.refreshAccessToken(success);
-	}
-	else {
-		var authorizationUrl = "https://accounts.google.com/o/oauth2/auth?client_id=" + this.clientId + "&redirect_uri=" + this.redirectUrl + "&scope=" + this.scope + "&response_type=code";
-		console.log("Authorisation URL=" + authorizationUrl);
-		// TODO = put part where we get the code from google authentication here (need to pop up window)
-		var data = {
-			code: "4/CIHw7LwXeHQSkuT49nnK3QNCL9Lq",
-			client_id: this.clientId,
-			client_secret: this.clientSecret,
-			redirect_uri: this.redirectUrl,
-			grant_type: "authorization_code"
-		};
-		var gt = this;
-		$.ajax({
-			url: "https://accounts.google.com/o/oauth2/token",
-			type: "POST",
-			dataType: 'json',
-			data: data,
-			success: function(data){
-				gt.access_token = data.access_token;
-				gt.refresh_token = data.refresh_token;
-				localStorage.setItem("GoogleRefreshKey", this.refresh_token);
-				console.log("got refresh token: " + this.refresh_token);
-				success();
-			}
-		});
-	}
-	return false;
-};
-
-GoogleTasks.prototype.refreshAccessToken = function(success){
-		var data = {
-		refresh_token:this.refresh_token,
-		client_id: this.clientId,
-		client_secret: this.clientSecret,
-		grant_type:"refresh_token"
+    var redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+    var scope = "https://www.googleapis.com/auth/tasks";
+	var access_token = "";
+	var refresh_token = "";
+	var authenticated = false;
+	var shoppingListName = "@default";
+	var eventServer = new EventEmitter();
+	var ACCESS_TOKEN_REFRESHED_EVENT = "GoogleTasks.ACCESS_TOKEN_REFRESHED";
+	var TASK_LIST_RETREIVED = "GoogleTasks.TASK_LIST_RETREIVED";
+	var kind= "tasks#task";
+	
+	var setTokens_fromAJAX = function(data){
+		access_token = data.access_token;
+		refresh_token = data.refresh_token;
+		localStorage.setItem("GoogleRefreshKey", refresh_token);
+		console.log("got refresh token: " + refresh_token);
+		authenticated = true;
+		eventServer.emit(ACCESS_TOKEN_REFRESHED_EVENT);
 	};
-	var gt = this;
-	$.ajax({
-  		url: "https://accounts.google.com/o/oauth2/token",
-		type: "POST",
-  		dataType: 'json',
-  		data: data,
-  		success: function(data){
-			gt.access_token = data.access_token;
-			console.log("got aceess token: " + gt.refresh_token);
-			success();
+	
+	var refreshAccessTokenByAJAX = function(data){
+		access_token = data.access_token;
+		authenticated = true;
+		console.log("got aceess token: " + refresh_token);
+		eventServer.emit(ACCESS_TOKEN_REFRESHED_EVENT);
+		
+	};
+	
+	var retreiveTaskListByAJAX = function(data){
+		console.log("TASK LIST\n------------------------\n\n"+JSON.stringify(data));
+		eventServer.emit(TASK_LIST_RETREIVED, data);
+	};
+    
+	var authenticate = function(){
+		var storedRefreshToken = localStorage.getItem("GoogleRefreshKey");
+		if (storedRefreshToken != undefined) {
+			refresh_token = storedRefreshToken;
+			refreshAccessToken();
 		}
-	});
-	return false;
+		else {
+			var authorizationUrl = "https://accounts.google.com/o/oauth2/auth?client_id=" + clientId + "&redirect_uri=" + redirectUrl + "&scope=" + scope + "&response_type=code";
+			console.log("Authorisation URL=" + authorizationUrl);
+			// TODO = put part where we get the code from google authentication here (need to pop up window)
+			var data = {
+				code: "4/VgBMa05qnEYqK4gxo2K2NUSB42Mp",
+				client_id: clientId,
+				client_secret: clientSecret,
+				redirect_uri: redirectUrl,
+				grant_type: "authorization_code"
+			};
+			var gt = this;
+			$.ajax({
+				url: "https://accounts.google.com/o/oauth2/token",
+				type: "POST",
+				dataType: 'json',
+				data: data,
+				success: setTokens_fromAJAX
+			});
+		}
+		return false;
+	};
 	
-}
+	var refreshAccessToken = function (){
+			var data = {
+			refresh_token:refresh_token,
+			client_id: clientId,
+			client_secret: clientSecret,
+			grant_type:"refresh_token"
+		};
+		$.ajax({
+	  		url: "https://accounts.google.com/o/oauth2/token",
+			type: "POST",
+	  		dataType: 'json',
+	  		data: data,
+	  		success: refreshAccessTokenByAJAX
+		});
+		return false;
+	};
+	
+	var isAuthenticated = function (){
+		return authenticated;
+	};
+	
+	var retreiveTaskList = function(){
+		var data = {
+			oauth_token: access_token,
+			prettyprint: false
+		};
+		$.ajax({
+	  		url: "https://www.googleapis.com/tasks/v1/lists/"+shoppingListName+"/tasks",
+			type: "GET",
+	  		dataType: 'json',
+	  		data: data,
+	  		success: retreiveTaskListByAJAX
+		});
+		return false;
+	}
+	
 
-GoogleTasks.prototype.isAuthenticated = function(){
+	return{  // return public API
+		ACCESS_TOKEN_REFRESHED_EVENT: ACCESS_TOKEN_REFRESHED_EVENT,
+		TASK_LIST_RETREIVED: TASK_LIST_RETREIVED,
+		authenticate: authenticate,
+		refreshAccessToken: refreshAccessToken,
+		isAuthenticated: isAuthenticated,
+		eventServer: eventServer,
+		retreiveTaskList: retreiveTaskList
+	};	
 	
-};
+}();
+
+
+
