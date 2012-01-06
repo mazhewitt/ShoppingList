5780ae51c5e140bcd008f92017077328b381898d
mazhewitt <mazda@aduki.co.uk>
2012-01-06 16:09:09 +0000

    fixed invalid character in tl

--- spec/GoogleTasksSpec.js
+++ spec/GoogleTasksSpec.js
@@ -1,6 +1,54 @@
 /**
  * @author mazda
  */
+ 
+
+
+ var GoogleTasksSpecHelper = {
+
+
+    setupFakeSuccessfulAuthServer: function(fakeserver) {
+        fakeserver.respondWith("POST", "https://accounts.google.com/o/oauth2/token", [200,
+        {
+            "content-type": "application/json; charset=UTF-8"
+        }, '{"access_token":"MY_STUB_ACCESS_TOKEN","expires_in":3920,"refresh_token":"MY_STUB_REFRESH_TOKEN"}']);
+    },
+    setupFakeSuccessfulTaskListServer: function(fakeserver, tasklist) {
+        fakeserver.respondWith("GET", "https://www.googleapis.com/tasks/v1/lists/@default/tasks?oauth_token=MY_STUB_ACCESS_TOKEN&prettyprint=false", [200,
+        {
+            "content-type": "application/json; charset=UTF-8"
+        },
+        tasklist]);
+    },
+    setupFakeFailingTaskListServer: function(fakeserver) {
+        fakeserver.respondWith("GET", "https://www.googleapis.com/tasks/v1/lists/@default/tasks?oauth_token=MY_STUB_ACCESS_TOKEN&prettyprint=false", [400,
+        {
+            "content-type": "application/json; charset=UTF-8"
+        }, ""]);
+    },
+    setupFakeFailingAuthServer: function(fakeserver) {
+        // now we need a fake XHR server to represent the authentication server
+        fakeserver.respondWith("POST", "https://accounts.google.com/o/oauth2/token", [400,
+        {
+            "content-type": "application/json; charset=UTF-8"
+        }, '']);
+
+    },
+    setupFakeSuccessfulTaskServer: function(fakeserver, task) {
+        fakeserver.respondWith("POST", "https://www.googleapis.com/tasks/v1/lists/@default/tasks", [200,
+        {
+            "content-type": "application/json; charset=UTF-8"
+        },      task]);
+        
+        var taskObj = jQuery.parseJSON(task);
+        
+        fakeserver.respondWith("DELETE", "https://www.googleapis.com/tasks/v1/lists/@default/tasks/"+taskObj.id, [200,
+        {
+            "content-type": "application/json; charset=UTF-8"
+        },      '']);
+    }
+};
+ 
 describe("GoogleTasksAPI", function() {
 
     var localStorage_get;
@@ -37,7 +85,7 @@ describe("GoogleTasksAPI", function() {
 
     it("can can retreive a task list from Google", function() {
         GoogleTasksSpecHelper.setupFakeSuccessfulAuthServer(fakeserver);
-        GoogleTasksSpecHelper.setupFakeSuccessfulTaskServer(fakeserver, tl);
+        GoogleTasksSpecHelper.setupFakeSuccessfulTaskListServer(fakeserver, tl);
         var gt = window.GoogleTasks;
         var taskList;
         gt.authenticate();
@@ -53,7 +101,7 @@ describe("GoogleTasksAPI", function() {
 
 
     it("can parse the mock tasklist", function() {
-        var thislist = GoogleTasksSpecHelper.tl;
+        var thislist = tl;
         var tasklist = jQuery.parseJSON(thislist);
         expect(tasklist).toBeDefined();
     });
@@ -72,7 +120,7 @@ describe("GoogleTasksAPI", function() {
     it("should raise an event when cannot get the shopping list ", function() {
         var spy = sinon.spy();
         GoogleTasksSpecHelper.setupFakeSuccessfulAuthServer(fakeserver);
-        GoogleTasksSpecHelper.setupFakeFailingTaskServer(fakeserver);
+        GoogleTasksSpecHelper.setupFakeFailingTaskListServer(fakeserver);
         var gt = window.GoogleTasks;
         gt.authenticate();
         fakeserver.respond();
@@ -93,62 +141,43 @@ describe("GoogleTasksAPI", function() {
 
     it("can add a task item", function() {
         GoogleTasksSpecHelper.setupFakeSuccessfulAuthServer(fakeserver);
-        GoogleTasksSpecHelper.setupFakeSuccessfulTaskServer(fakeserver, tl);
+        GoogleTasksSpecHelper.setupFakeSuccessfulTaskServer(fakeserver, testTask);
         var gt = window.GoogleTasks;
         gt.authenticate();
         fakeserver.respond();
         expect(gt.isAuthenticated()).toBeTruthy();
         var newTask = {
-            "title": "Test Task",
-            "notes": "Some Notes",
-            "status": "",
-            "due": "",
-            "completed": false
+            title: "Pork Chops",
+            notes: "Get 20"
         };
-        gt.insert(newTask);
+        var responseTask;
+        gt.eventServer.on(gt.TASK_INSERTED, function(data){
+            responseTask = data;
+        });
+        gt.insertTask(newTask);
         fakeserver.respond();
-        gt.eventServer.on(gt.TASK_INSERTED, spy);
-        expect(spy).toHaveBeenCalled();
+        expect(responseTask.title).toEqual(newTask.title);
+        expect(responseTask.updated).toBeDefined();
     });
 
 
 });
 
-var GoogleTasksSpecHelper = {
+var testTask = '\
+{\
+ "kind": "tasks#task",\
+ "id": "MDQ0MjgwNjg1MDI3MjgyMzA1NDk6MDoxMzAyOTUwNTc1",\
+ "etag": "\\"Eb0IVTIf8nvTXDAzt6s5r2fCyaI/LTk2NTg0MjkyOA\\"",\
+ "title": "Pork Chops",\
+ "updated": "2012-01-06T13:29:57.000Z",\
+ "selfLink": "https://www.googleapis.com/tasks/v1/lists/MDQ0MjgwNjg1MDI3MjgyMzA1NDk6MDow/tasks/MDQ0MjgwNjg1MDI3MjgyMzA1NDk6MDoxMzAyOTUwNTc1",\
+ "position": "00000000000025873296",\
+ "notes": "Get 20",\
+ "status": "needsAction"\
+}';
 
 
-    setupFakeSuccessfulAuthServer: function(fakeserver) {
-        fakeserver.respondWith("POST", "https://accounts.google.com/o/oauth2/token", [200,
-        {
-            "content-type": "application/json; charset=UTF-8"
-        }, '{"access_token":"MY_STUB_ACCESS_TOKEN","expires_in":3920,"refresh_token":"MY_STUB_REFRESH_TOKEN"}']);
-    },
-    setupFakeSuccessfulTaskServer: function(fakeserver, tasklist) {
-        fakeserver.respondWith("GET", "https://www.googleapis.com/tasks/v1/lists/@default/tasks?oauth_token=MY_STUB_ACCESS_TOKEN&prettyprint=false", [200,
-        {
-            "content-type": "application/json; charset=UTF-8"
-        },
-        tasklist]);
-    },
-    setupFakeFailingTaskServer: function(fakeserver) {
-        fakeserver.respondWith("GET", "https://www.googleapis.com/tasks/v1/lists/@default/tasks?oauth_token=MY_STUB_ACCESS_TOKEN&prettyprint=false", [400,
-        {
-            "content-type": "application/json; charset=UTF-8"
-        }, ""]);
-    },
-    setupFakeFailingAuthServer: function(fakeserver) {
-        // now we need a fake XHR server to represent the authentication server
-        fakeserver.respondWith("POST", "https://accounts.google.com/o/oauth2/token", [400,
-        {
-            "content-type": "application/json; charset=UTF-8"
-        }, '']);
-
-    }
-
-
-};
-
-var tl = '\
+var    tl= '\
 {\
  "kind": "tasks#tasks",\
  "etag": "\\"4I1JyCN_AcgBElJWYb7lv8jetTk/xnx8JAF-AwgNU5cDVK3VZb2dTuk\\"",\
@@ -172,7 +201,7 @@ var tl = '\
   },{\
    "kind": "tasks#task",\
    "id": "MDQ0MjgwNjg1MDI3MjgyMzA1NDk6Mjoz",\
-   "title": "item 3",\ 
+   "title": "item 3",\
    "updated": "2011-06-04T20:20:48.485Z",\
    "selfLink": "https://www.googleapis.com/tasks/v1/lists/MDQ0MjgwNjg1MDI3MjgyMzA1NDk6Mjow/tasks/MDQ0MjgwNjg1MDI3MjgyMzA1NDk6Mjoz",\
    "position": "00000000003758096383",\
